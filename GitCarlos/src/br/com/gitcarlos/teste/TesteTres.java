46834dd61e042ea94791e1922df758d6dffc1de0
CarlosQw32 <ncarlos63@gmail.com>
2023-06-07 11:32:17 -0300

    Teste 3

---
+++ GitCarlos/src/br/com/gitcarlos/teste/TesteTres.java
@@ -0,0 +1,10 @@
+package br.com.gitcarlos.teste;
+
+public class TesteTres {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Teste 3");
+	}
+
+}
